38e62d274c26a880efeac9cb1d477e1f5dd71262
setinajb <setinajb@rose-hulman.edu>
2018-01-28 22:10:58 -0500

    working on m4

--- src/m4_sequences.py
+++ src/m4_sequences.py
@@ -5,8 +5,8 @@ This problem provides practice at:
   ***  SEQUENCES.  ***
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Jaclyn Setina.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 ########################################################################
 # Students:
@@ -134,14 +134,18 @@ def practice_problem4a(sequence):
       :type sequence: list | tuple | string
     """
     ####################################################################
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #     The testing code is already written for you (above).
     ####################################################################
     # DIFFICULTY AND TIME RATINGS (see top of this file for explanation)
     #    DIFFICULTY:      7
     #    TIME ESTIMATE:   15 minutes.
     ####################################################################
-
+    new_list = []
+    for k in range(0, len(sequence)-1):
+        if sequence[k] == sequence[k+1]:
+            new_list = new_list + [k]
+    return new_list
 
 def run_test_practice_problem4b():
     """ Tests the    practice_problem4b    function. """
@@ -197,13 +201,18 @@ def practice_problem4b(sequence):
       :type sequence: (list | tuple) of (float | int)
     """
     # ------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #     The testing code is already written for you (above).
     ####################################################################
     # DIFFICULTY AND TIME RATINGS (see top of this file for explanation)
     #    DIFFICULTY:      5
     #    TIME ESTIMATE:   10 minutes.
     ####################################################################
+    start_seq = -math.inf
+    for k in range(0, len(sequence), 2):
+        if sequence[k] > start_seq:
+            start_seq = sequence[k]
+    return start_seq
 
 
 def run_test_practice_problem4c():
@@ -295,7 +304,7 @@ def practice_problem4c(points):
       :rtype: rg.Point | string
     """
     ####################################################################
-    # TODO: 4. Implement and test this function.
+    # DONE: 4. Implement and test this function.
     #     The testing code is already written for you (above).
     #
     # IMPORTANT: This problem is your LOWEST PRIORITY for preparing
@@ -306,6 +315,16 @@ def practice_problem4c(points):
     #    DIFFICULTY:      9
     #    TIME ESTIMATE:   15 minutes.
     ####################################################################
+    for point in points:
+        if is_prime(point.x) and is_prime(point.y):
+            temp = point.x
+            point.x = point.y
+            point.y = temp
+            return point
+
+    return 'Not found'
+
+
 
 
 def run_test_practice_problem4d():
@@ -398,6 +417,8 @@ def practice_problem4d(sequence):
     #    DIFFICULTY:      7
     #    TIME ESTIMATE:   15 minutes.
     ####################################################################
+
+
 
 
 # ----------------------------------------------------------------------
