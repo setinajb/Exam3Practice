d64924a07e859f3a3b0f46e444573b8deeeb3a82
setinajb <setinajb@rose-hulman.edu>
2018-01-28 21:45:14 -0500

    finished m3

--- src/m3_loops.py
+++ src/m3_loops.py
@@ -5,8 +5,8 @@ This problem provides practice at:
   ***  FOR and WHILE loops.  ***
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Jaclyn Setina.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 ########################################################################
 # Students:
@@ -45,7 +45,7 @@ def main():
 def run_test_practice_problem3():
     """ Tests the   practice_problem3  function. """
     ####################################################################
-    # TODO: 2. Implement this TEST function.
+    # DONE: 2. Implement this TEST function.
     #   It TESTS the  practice_problem3  function defined below.
     #   Include at least ** 2 ** ADDITIONAL tests beyond those we wrote.
     #
@@ -109,6 +109,12 @@ def run_test_practice_problem3():
              st.SimpleTestCase(practice_problem3,
                                [0, 1, 1.414213562373],
                                [286602]),
+             st.SimpleTestCase(practice_problem3,
+                               [-1, 0, 3],
+                               []),
+             st.SimpleTestCase(practice_problem3,
+                               [-4, -3, -1.0],
+                               []),
              ]
     # 14th test:
     big_list = []
@@ -208,7 +214,7 @@ def practice_problem3(start, n, threshold):
       :type threshold: float
     """
     ####################################################################
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #          Some tests are already written for you (above),
     #          but you are required to write ADDITIONAL tests (above).
     ####################################################################
@@ -216,7 +222,18 @@ def practice_problem3(start, n, threshold):
     #    DIFFICULTY:      5
     #    TIME ESTIMATE:   < 15 minutes.
     ####################################################################
+    import math
+    new_list = []
+    num_results = 0
+    k = start
+    while num_results < n:
+        total = math.sin(k) + math.cos(k)
+        if total > threshold:
+            new_list = new_list + [k]
+            num_results = num_results + 1
 
+        k = k + 1
+    return new_list
 
 # ----------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
